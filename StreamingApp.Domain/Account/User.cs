0ab5f8b8d06c6896b83bf8b06bffc1cf3b2b3f14
Vikmcr99 <victor.robinson@al.infnet.edu.br>
2024-07-23 11:16:41 -0300

    Criando as regras de negócios do Usuario

--- StreamingApp.Domain/Account/User.cs
+++ StreamingApp.Domain/Account/User.cs
@@ -16,6 +16,52 @@ namespace StreamingApp.Domain.Account
 
         public List<Playlist> Playlists { get; set; } = new List<Playlist>();
 
-        public List<Subscription> Subscription { get; set; } = new List<Subscription>();
+        public List<Subscription> Subscriptions { get; set; } = new List<Subscription>();
+
+        public void CreateUser(string name, Plan plan, Card card)
+        {
+            this.Name = name;
+            this.SubscrivePlan(plan, card);
+            this.AddCard(card);
+            this.CreatePlaylist();
+        }
+
+        private void CreatePlaylist(string name = "Favorites", bool publicPlaylist = false)
+        {
+            this.Playlists.Add(new Playlist
+            {
+                Id = new Guid(),
+                Name = name,
+                Public = publicPlaylist,
+                User = this
+
+            });
+        }
+
+        private void AddCard(Card card)
+        {
+           this.Cards.Add(card);   
+        }
+
+        private void SubscrivePlan(Plan plan, Card card)
+        {
+            card.CreateTransaction(plan.Name, plan.Value, plan.Description);
+
+            if (this.Subscriptions.Count > 0 && this.Subscriptions.Any(x => x.Active))
+            {
+                var activePlan = this.Subscriptions.FirstOrDefault(x => x.Active);
+                activePlan.Active = false;
+            }
+
+            this.Subscriptions.Add(new Subscription()
+            {
+                Active = true,
+                Date =  DateTime.Now,
+                Plan = plan,
+                Id = new Guid()
+
+            });
+
+        }
     }
 }
